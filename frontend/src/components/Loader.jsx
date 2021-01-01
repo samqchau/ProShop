6e779d9ec1ce33fb3308382488a540aad6d9cb6e
samqchau <67344952+samqchau@users.noreply.github.com>
2020-12-31 18:57:39 -0800

    Product Details - Redux
    -Created loading and error message components to display if redux state is 'loading' or returns an error
    
    -Product details page now uses product state from redux by passing the listProductDetails action from the productActions module.
    
    *Order to make redux state for a component
    1. Define constants (action type for reducer)
    2. Make reducer function (how to change state for each action type case)
    3. Add reducer function to store
    4. Write action handling function to handle dispatching the reducer in logical order
    5. Add and deconstruct redux state (from store's combineReducers) to component with useSelector
    6. Dispatch action handling function with useDispatch from component

---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Spinner, Container, Row } from 'react-bootstrap';
+
+const Loader = () => {
+  return (
+    <Container>
+      <Row>
+        <Spinner
+          animation='border'
+          variant='info'
+          role='status'
+          style={{
+            width: '25px',
+            height: '25px',
+            display: 'block',
+            position: 'absolute',
+            left: '50%',
+            top: '50%',
+          }}
+        >
+          <span class='sr-only'>Loading...</span>
+        </Spinner>
+      </Row>
+    </Container>
+  );
+};
+
+export default Loader;
