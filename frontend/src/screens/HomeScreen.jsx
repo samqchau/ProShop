b084e17dc6209ca9e2dec0adbe834f7fad7d1a5e
samqchau <67344952+samqchau@users.noreply.github.com>
2020-12-31 17:31:13 -0800

    More Redux
    Connected all the moving parts of redux for the products in the store
    
    Reducers - change and modify state in a predictable way based on action type. The reducer function recieves the action.
    
    Actions - The action object is dispatched to the reducer function. The action has a type that tells the reducer how to modify the state and the payload that holds data the reducer can use.
    
    Dispatchers - Dispatch passes the action to the reducer function
    
    We keep our actions, reducers, and constants in separate folders for organization. They are all connected in the redux store where all the reducers are combined with the combineReducers function from 'redux' .
    
    The global redux state and reducers can be accessed using the useSelector and useDispatch hooks from 'react-redux'

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,30 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
 import Product from '../components/Product';
-import axios from 'axios';
+import { listProducts } from '../actions/productActions.js';
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { products, loading, error } = productList;
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h1>Loading</h1>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
