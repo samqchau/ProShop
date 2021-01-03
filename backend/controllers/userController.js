529d968e11eed81337ca7c545ec0af6c22af4f79
samqchau <67344952+samqchau@users.noreply.github.com>
2021-01-02 20:22:53 -0800

    JSON Web Token
    -installed jsonwebtoken and used it to generate a token when user logs in (POST /api/users/login)
    -wrote middleware called protect that authorizes users by checking jwt in req.headers.authorize. if token is authorized we call the next middleware which takes the id property from the parsed token and finds the user in the database returning all the user's information other than the protect password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
 //@desc Auth user & send token (for visiting protected routes)
 //@route Post /api/users/login
@@ -13,10 +14,29 @@ export const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
     throw new Error('Invalid email or password');
+  }
+});
+
+//@desc Get user profile
+//@route GET /api/users/profile
+//@access Private
+
+export const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
   }
 });
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { authUser } from '../controllers/userController.js';
+import { authUser, getUserProfile } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/login', authUser);
+userRouter.get('/profile', protect, getUserProfile);
 
 export default userRouter;
