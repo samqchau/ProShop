6e7ecf8e54869c21cd91d1fc137be3782ba38b98
samqchau <67344952+samqchau@users.noreply.github.com>
2021-01-02 20:57:06 -0800

    Register new user (backend)
    -Implemented backend functionality for registering a new user with a name, email, and password.
    -POST '/api/users'
    -Bcrypt was used to salt and hash the password pre save in the User Model. (if password was not modified call next middleware). *model.create({ value:value })

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import { authUser, getUserProfile } from '../controllers/userController.js';
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+} from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const userRouter = express.Router();
 
+userRouter.post('/', registerUser);
 userRouter.post('/login', authUser);
 userRouter.get('/profile', protect, getUserProfile);
 
