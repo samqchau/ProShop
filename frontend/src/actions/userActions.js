82c6761653946e3beefac373647e2556d9e3f93b
samqchau <67344952+samqchau@users.noreply.github.com>
2021-01-03 18:23:56 -0800

    Frontend Logout
    Navbar login button now changes to drop down menu with username upon sign in.
    
    Logout button added to remove userinfo from local storage and change user state back to an empty object

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,6 +3,7 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
+  USER_LOGOUT,
 } from '../constants/userConstants';
 
 export const login = (email, password) => async (dispatch, getState) => {
@@ -32,4 +33,9 @@ export const login = (email, password) => async (dispatch, getState) => {
           : error.message,
     });
   }
+};
+
+export const logout = () => async (dispatch, getState) => {
+  localStorage.removeItem('userInfo');
+  dispatch({ type: USER_LOGOUT });
 };
---
+++ frontend/src/screens/RegisterScreen.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {} from 'react-bootstrap';
+
+const RegisterScreen = () => {
+  return <div></div>;
+};
+
+export default RegisterScreen;
