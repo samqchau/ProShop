b7cfbb926c94f366062c0bec4249ed26afe046e9
samqchau <67344952+samqchau@users.noreply.github.com>
2021-01-09 22:15:22 -0800

    GET & PUT '/api/users/:id'
    Implemented route and controller at '/api/users/:id' for admins to access and update a single user's information.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,17 +6,22 @@ import {
   updateUserProfile,
   getAllUsers,
   deleteUser,
+  getUserById,
+  updateUserById,
 } from '../controllers/userController.js';
 import { protect, adminOnly } from '../middleware/authMiddleware.js';
 
 const userRouter = express.Router();
 // '/api/users'
-userRouter.get('/', protect, adminOnly, getAllUsers);
-userRouter.post('/', registerUser);
+userRouter.get('/', protect, adminOnly, getAllUsers).post('/', registerUser);
 userRouter.post('/login', authUser);
-userRouter.get('/profile', protect, getUserProfile);
-userRouter.put('/profile', protect, updateUserProfile);
+userRouter
+  .get('/profile', protect, getUserProfile)
+  .put('/profile', protect, updateUserProfile);
 
-userRouter.delete('/:id', protect, adminOnly, deleteUser);
+userRouter
+  .delete('/:id', protect, adminOnly, deleteUser)
+  .get('/:id', protect, adminOnly, getUserById)
+  .put('/:id', protect, adminOnly, updateUserById);
 
 export default userRouter;
