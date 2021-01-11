78cc5975996eb8209cd2090892054d0734a10d18
samqchau <67344952+samqchau@users.noreply.github.com>
2021-01-10 21:19:46 -0800

    POST /api/products PUT api/products/:id
    Created Routes and Controllers for creating a new sample product and updating an existing product. The sample product will be used when creating a new product redirecting the admin to a form page with the sample product information where they can edit the fields and update it.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -36,3 +36,56 @@ export const deleteProduct = asyncHandler(async (req, res) => {
     throw new Error('Product not found');
   }
 });
+
+//@desc Create a product
+//@route POST /api/products/
+//@access Private/Admin only
+export const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'Sample name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description',
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+//@desc Update a product
+//@route PUT /api/products/:id
+//@access Private/Admin only
+export const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    image,
+    brand,
+    category,
+    countInStock,
+    description,
+  } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name || product.name;
+    product.price = price || product.price;
+    product.image = image || product.image;
+    product.brand = brand || product.brand;
+    product.category = category || product.category;
+    product.countInStock = countInStock || product.countInStock;
+    product.description = description || product.description;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(401);
+    throw new Error('Product not found');
+  }
+});
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,14 +3,18 @@ import {
   getProducts,
   getProductById,
   deleteProduct,
+  updateProduct,
+  createProduct,
 } from '../controllers/productController.js';
 
 import { protect, adminOnly } from '../middleware/authMiddleware.js';
 
 const productRouter = express.Router();
-productRouter.get('/', getProducts);
+productRouter
+  .get('/', getProducts)
+  .post('/', protect, adminOnly, createProduct);
 productRouter
   .get('/:id', getProductById)
+  .put('/:id', protect, adminOnly, updateProduct)
   .delete('/:id', protect, adminOnly, deleteProduct);
-
 export default productRouter;
